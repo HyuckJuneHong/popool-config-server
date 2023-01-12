f22b108031a8b15e7c4fd469470eec8d8671a05d
홍혁준 <jjj8337721@naver.com>
2023-01-13 01:07:28 +0900

    feat : init commit

---
+++ src/main/java/kr/co/popoolconfigserver/PopoolConfigServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.popoolconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopoolConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopoolConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/popoolconfigserver/PopoolConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.popoolconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopoolConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
