c78b0e8b1e14754e53420c3840b10c17be5a2a8d
홍혁준 <jjj8337721@naver.com>
2023-01-13 01:10:18 +0900

    feat : enable config server

--- src/main/java/kr/co/popoolconfigserver/PopoolConfigServerApplication.java
+++ src/main/java/kr/co/popoolconfigserver/PopoolConfigServerApplication.java
@@ -2,8 +2,10 @@ package kr.co.popoolconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class PopoolConfigServerApplication {
 
     public static void main(String[] args) {
